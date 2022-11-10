03d460e4a8936ff560ea27181f4fb21781296e0c
tusharkarma <117929306+tusharkarma@users.noreply.github.com>
2022-11-10 21:47:34 +0530

    Create call by value.cpp

---
+++ call by value.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int call_by_value(int n)
+{
+	cout<<"address of n is call_by_function : "<<&n<<endl;
+	n*=n;
+	return n;
+}
+int main()
+{
+	int n=8;
+	cout<<"address of n in main() : "<<&n<<endl;
+	cout<<"ans : "<<call_by_value(n)<<endl;
+	cout<<"value of n : "<<n<<endl;
+	return 0;
+}
