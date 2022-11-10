ac91853d879a8fad8204182e8d6c0216aaa326f9
tusharkarma <117929306+tusharkarma@users.noreply.github.com>
2022-11-10 21:45:06 +0530

    Create array vs pointer.cpp

---
+++ array vs pointer.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int ar[5]={10,20,30,40,50};
+	cout<<ar<<endl;
+	cout<<&ar[0]<<endl;
+	cout<<&ar[1]<<endl;
+	cout<<ar[0]<<endl;
+	cout<<ar[1];
+	
+}
